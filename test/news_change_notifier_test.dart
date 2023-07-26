15df167914174a0cae08c55ad080610910599a13
Christian Bondzie <christian.zap@hotmail.fr>
2023-07-26 14:38:08 +0200

    Etablissement de test unitaire pour la methode

--- test/news_change_notifier_test.dart
+++ test/news_change_notifier_test.dart
@@ -4,6 +4,7 @@ import 'package:flutter_testing_tutorial/news_change_notifier.dart';
 import 'package:flutter_testing_tutorial/news_service.dart';
 import 'package:mocktail/mocktail.dart';
 
+// Mock est une simulation d'un objet réel il remplace les dépendances externe à la fonction que l'on souhaite tester.
 class MockNewsService extends Mock implements NewsService {}
 
 void main() {
@@ -15,31 +16,27 @@ void main() {
     sut = NewsChangeNotifier(mockNewsService);
   });
 
-  test(
-    "initial values are correct",
-    () {
-      expect(sut.articles, []);
-      expect(sut.isLoading, false);
-    },
-  );
+  test('initial value are correct', () {
+    expect(sut.articles, []);
+    expect(sut.isLoading, false);
+  });
 
   group('getArticles', () {
-    final articlesFromService = [
+    final testValue = [
       Article(title: 'Test 1', content: 'Test 1 content'),
       Article(title: 'Test 2', content: 'Test 2 content'),
-      Article(title: 'Test 3', content: 'Test 3 content'),
+      Article(title: 'Test 3', content: 'Test 3 content')
     ];
-
-    void arrangeNewsServiceReturns3Articles() {
-      when(() => mockNewsService.getArticles()).thenAnswer(
-        (_) async => articlesFromService,
-      );
+    
+    void arrangeNewsServiceReturns3articles() {
+      when(() => mockNewsService.getArticles())
+          .thenAnswer((_) async => testValue);
     }
 
     test(
-      "gets articles using the NewsService",
+      "gets articles using the newService",
       () async {
-        arrangeNewsServiceReturns3Articles();
+        arrangeNewsServiceReturns3articles();
         await sut.getArticles();
         verify(() => mockNewsService.getArticles()).called(1);
       },
@@ -48,13 +45,19 @@ void main() {
     test(
       """indicates loading of data,
       sets articles to the ones from the service,
-      indicates that data is not being loaded anymore""",
+      the data nobeing loaded anymore""",
       () async {
-        arrangeNewsServiceReturns3Articles();
+        // on arrage la valeur de get articles à vérifier ici on poste 3 articles
+        arrangeNewsServiceReturns3articles();
+        // On appel la fonction getArticle mais on ne l'attend pas car le process serait fini
         final future = sut.getArticles();
+        // on verfie que loading est passé à vrai
         expect(sut.isLoading, true);
+        // on attends que le process fini
         await future;
-        expect(sut.articles, articlesFromService);
+        // on s'attend a ce que sut.article soit égale aux valeur arbitraires
+        expect(sut.articles, testValue);
+        // on vérifie que le loading repasse a faux en fin de process
         expect(sut.isLoading, false);
       },
     );
